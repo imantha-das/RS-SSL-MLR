9924be24966b3d838d05303cf6f2e93b8a2b47a2
imantha-das <imantha@yahoo.com>
2024-10-16 19:01:14 +0800

    updates to utils.py

--- src/models/utils.py
+++ src/models/utils.py
@@ -10,12 +10,12 @@ from torch.utils.data import Dataset
 from torchvision.transforms import ToTensor, Normalize, Compose 
 from torch.utils.data import DataLoader
 
-import cv2
-from PIL import Image
+import rasterio
+
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import make_column_transformer
 
-from typing import List
+from typing import List, Tuple
 
 from termcolor import colored
 from tqdm import tqdm
@@ -81,16 +81,15 @@ def remove_images(paths:list):
     print(f"Clean image count : {nd_array_cnt}")
     print(f"Damaged images : {other_cnt}")
 
-# ------------ Dataset Class for Remote Sensing Images of Malaysia ----------- #
+# ---------------------- Dataset Class for Drone Images ---------------------- #
 
-#todo This use Scikit-Image instead try and ammend it to OpenCV
-class SSHSPH_MY(Dataset):
+class DroneDataset(Dataset):
     """
     Desc : Dataset class for loading Remote Sensing Images (captured in Malaysia)
     The respository contain .jpg images with no labels. Hence the dataset will
     only return values for X.
     """
-    def __init__(self, paths:List[str], transform:Compose = None):
+    def __init__(self, paths:List[str], transform:Compose = None, scaling_factor = None)->Tuple[torch.Tensor, str]:
         """
         Desc : Initialization
         Parameters
@@ -98,6 +97,7 @@ class SSHSPH_MY(Dataset):
         """
         self.paths = paths
         self.transform = transform 
+        self.scaling_factor = scaling_factor
 
     def __len__(self):
         """
@@ -112,18 +112,62 @@ class SSHSPH_MY(Dataset):
         Parameters :
             - idx : index
         """
-        image_path = self.paths[idx]
-        image = np.asarray(Image.open(image_path)) #(512,512,3)
-        image_copy = np.copy(image) # Need to copy image to make it writable
+        with rasterio.open(self.paths[idx]) as ds:
+            img = ds.read([1,2,3]) # (C,W,H)
+
+        # convert to torch tensor : We havent used torchvision.ToTensor function as it automaticall normalize to range 0-1
+        img = torch.tensor(img, dtype = torch.float32) #(C,W,H)
+        # apply scaling factor if define, i.e 255 since drone images are within 0-255 range
+        if self.scaling_factor:
+            img = img/self.scaling_factor 
 
         if self.transform:
-            return self.transform(image_copy)
-        else:
-            return image_copy.astype("float")
+            img = self.transform(img)
+
+        return img, self.paths[idx]
+    
+# --------------------- Dataset Class for Sentinel Images -------------------- #
+class SentinelDataset(Dataset):
+    """Desc : Dataset class for satelite data downloaded from google eath engine"""
+    def __init__(self, paths:List[str], transform:Compose = None, scaling_factor = None)-> Tuple[torch.Tensor,str]:
+        """
+        Inputs 
+             - paths : List of image paths
+             - transform : Any Pytorch transformations
+        NOTE : GEE states to divide dataset by 10,000 however values are difficult to be visualized, 
+        so we will normalize the values btw 0 and 1
+        """
+        self.paths = paths
+        self.transform = transform
+        self.scaling_factor = scaling_factor
+
+    def __len__(self):
+        """Desc : Return dataset size"""
+        return len(self.paths)
+    
+    def __getitem__(self, idx:int):
+        """
+        Desc : Return a single item Based on Index
+        Inputs 
+            - idx : index
+        """
+        with rasterio.open(self.paths[idx]) as ds:
+            img = ds.read([1,2,3]) #(C,W,H)
+
+        # convert to torch tensor : We havent used torchvision.ToTensor function as it automaticall normalize to range 0-1 
+        img = torch.tensor(img, dtype = torch.float32) # (C,W,H)
+        if self.scaling_factor:
+            img = img / self.scaling_factor
+
+        if self.transform:
+            img = self.transform(img)
+
+        return img, self.paths[idx]
 
  
 # ------------ Dataset Class for Remote Sensing Images of Malaysia ----------- #
 
+#todo : We need to incorporate rasterio
 class SSHSPH_MALARIA_MY(Dataset):
     """
     Construct a Dataset for Malaria Data (i.e points on csv + images)
@@ -185,17 +229,21 @@ class DATASET_MEAN_STD():
         """
         Computes the mean and std for SSHSPH_MY dataset
         """
-        c_sum, c_sq_sum, num_batches = 0,0,0
+        c_sum, c_sq_sum, total_pixels = 0,0,0
 
-        d = self.DATASET(image_paths, transform = Compose([ToTensor()]))
+        d = self.DATASET(image_paths)
         dl = DataLoader(d, batch_size = self.batch_size)
-        for data in tqdm(dl): # note here we dont have traget else should be (data,labs)
-            c_sum += torch.mean(data, dim = [0,2,3]) # Sum across channels, data.shape = (b,c,w,h)
-            c_sq_sum += torch.mean(data**2 , dim = [0,2,3])
-            num_batches += 1
+        for data,path in tqdm(dl): # note here we dont have traget else should be (data,labs)
+            b,c,w,h = data.shape
+            num_pixels = b * w * h
+            assert c == 3, f"Num channels incorrect, got c = {c}"
+            c_sum += torch.sum(data, dim = [0,2,3]) # Sum across channels, data.shape = (b,c,w,h)
+            c_sq_sum += torch.sum(data**2 , dim = [0,2,3])
+
+            total_pixels += num_pixels
         
-        mean = c_sum / num_batches
-        std = (c_sq_sum/num_batches - mean**2)**0.5
+        mean = c_sum / total_pixels
+        std = ((c_sq_sum/total_pixels) - (mean**2))**0.5
 
         return mean,std
 
@@ -207,17 +255,20 @@ class DATASET_MEAN_STD():
             - df : Dataframe containing points + image_paths + features
             - targets : target variable, not needed for this function except to pass to SSHSPH_MALARIA_MY dataset class
         """
-        c_sum, c_sq_sum, num_batches = 0,0,0
+        c_sum, c_sq_sum, total_pixels = 0,0,0
 
-        d = self.DATASET(df, target, transform = Compose([ToTensor()]))
+        d = self.DATASET(df, target)
         dl = DataLoader(d, batch_size = self.batch_size)
         for data,_,_ in  tqdm(dl): # we have both imgs and labs for this dataset class
-            c_sum += torch.mean(data, dim = [0,2,3]) # Sum across channels, data.shape = (b,c,w,h)
-            c_sq_sum += torch.mean(data**2 , dim = [0,2,3])
-            num_batches += 1
+            b,c,w,h = data.shape
+            num_pixels = b * w * h # total pixels over batch per channel
+            c_sum += torch.sum(data, dim = [0,2,3]) # Sum across channels, data.shape = (b,c,w,h)
+            c_sq_sum += torch.sum(data**2 , dim = [0,2,3])
+            
+            total_pixels += num_pixels # aggregate num_pixels (batch) over entire dataset 
 
-        mean = c_sum / num_batches
-        std = (c_sq_sum/num_batches - mean**2)**0.5
+        mean = c_sum / total_pixels
+        std = ((c_sq_sum/total_pixels) - (mean**2))**0.5
 
         return mean,std
 
