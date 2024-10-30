d55227036acb2c175ecdf8b9e399e7bdc686f536
imantha-das <imantha@yahoo.com>
2024-10-30 23:40:17 +0800

    adding jobscript.sh

---
+++ jobscript.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+## Resources to be used
+#PBS -l select=1:ngpus=4
+
+## Walltime for the project to be run
+#PBS -l walltime=01:00:00
+
+## Use the AI cluster
+#PBS -q ai
+
+## Use project
+#PBS -P 11003907
+
+## Job name
+# PBS -N byol_test
+
+## Merge standard output and error from PBS script
+#PBS -j oe
+
+## Code starts from login nodes directory once logged into a compute node
+cd ${PBS_O_WORKDIR}
+
+## Load minmiforge3
+module load miniforge3
+conda activate rs-ssl-mlr
+## Run python script
+python /home/users/nus/idg/workspace/RS-SSL-MLR/src/models/train_byol.py -data_fold_drn  /home/users/nus/idg/scratch/data/processed/sshsph_drn/c3_256x_pch -data_fold_sat /home/users/nus/idg/scratch/data/interim/gee_sat/sen2a_c3_256x_clp0.3_uint8_ucln_pch -pretrain_weight_file /home/users/nus/idg/workspace/RS-SSL-MLR/model_weights/rsp_weights/rsp-aid-resnet-50-e300-ckpt.pth -save_weights_fold /home/users/nus/idg/workspace/RS-SSL-MLR/model_weights/ssl_weights
+
+
+
