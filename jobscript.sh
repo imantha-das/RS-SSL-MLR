98a32ceb09ef05d14cb9d046e7d182425ed6d3a7
imantha-das <imantha@yahoo.com>
2024-10-31 22:09:09 +0800

    jobscript for simsiam

--- jobscript.sh
+++ jobscript.sh
@@ -23,9 +23,9 @@ cd ${PBS_O_WORKDIR}
 
 ## Load minmiforge3
 module load miniforge3
-conda activate rs-ssl-mlr
+conda activate ssl-env
 ## Run python script
-python /home/users/nus/idg/workspace/RS-SSL-MLR/src/models/train_byol.py -data_fold_drn  /home/users/nus/idg/scratch/data/processed/sshsph_drn/c3_256x_pch -data_fold_sat /home/users/nus/idg/scratch/data/interim/gee_sat/sen2a_c3_256x_clp0.3_uint8_ucln_pch -pretrain_weight_file /home/users/nus/idg/workspace/RS-SSL-MLR/model_weights/rsp_weights/rsp-aid-resnet-50-e300-ckpt.pth -save_weights_fold /home/users/nus/idg/workspace/RS-SSL-MLR/model_weights/ssl_weights
+python /home/users/nus/idg/workspace/RS-SSL-MLR/src/models/simsiam_train.py -data_fold_drn  /home/users/nus/idg/scratch/data/processed/sshsph_drn/c3_256x_pch -data_fold_sat /home/users/nus/idg/scratch/data/interim/gee_sat/sen2a_c3_256x_clp0.3_uint8_ucln_pch -pretrain_weight_file /home/users/nus/idg/workspace/RS-SSL-MLR/model_weights/pretrain_weights/rsp-aid-resnet-50-e300-ckpt.pth -save_weights_fold /home/users/nus/idg/workspace/RS-SSL-MLR/model_weights/ssl_weights
 
 
 
