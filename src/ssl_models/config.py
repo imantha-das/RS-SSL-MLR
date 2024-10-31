129660647d8105ec57319c6bc127294366791ba3
imantha-das <imantha@yahoo.com>
2024-10-31 22:05:29 +0800

    updates to SimSiam

--- src/ssl_models/config.py
+++ src/ssl_models/config.py
@@ -1,20 +1,19 @@
 # Training Hyperparameters
 BATCH_SIZE = 128
 INPUT_SIZE = 256
-MAX_EPOCHS = 2
-# Normalizing Values after Scaling
-sen2a_scaled_img_mean = [0.0738, 0.0956, 0.1218] ; sen2a_scaled_img_std = [0.1069, 0.0973, 0.0943]
-drn_scaled_img_mean = [0.4768, 0.5559, 0.4325] ; drn_scaled_img_std = [0.1557, 0.1466, 0.1245] 
-# Normalizing values before scaling - normalizing values for Raw input
-sen2a_raw_img_mean = [ 737.9805,  956.4913, 1218.3508] ; sen2a_raw_img_std = [1069.0999,  973.3830,  942.9846]
-drn_raw_img_mean = [121.5762, 141.7526, 110.2758]; drn_raw_img_std = [39.7072, 37.3879, 31.7408]
+MAX_EPOCHS = 1
+# Normalizing values for Satelites
+sat_img_mean = [0.2132, 0.2890, 0.3737] ; sat_img_std = [0.2092, 0.1928, 0.1706]
+# Normalizing values for Drones
+drn_img_mean = [0.4768, 0.5559, 0.4325]; drn_img_std = [0.1557, 0.1466, 0.1245]
 
 SENTINEL_SR_SF = 10000 # SR vqlues are scaled by 10,000 and hence need to divide by this value.
 DRONE_SF = 255 # Divide by this nuber to scale to 0-1
 Z_DIMS = 128 # Embedding size for simCLR hidden state (zi)
 LR = 0.0005 * BATCH_SIZE / 256 # For Dino use formulae : 0.0005 * batch_size / 256
-LR_SCHEDULE = True # True if you want to use a lr_scheduler
+APPLY_LR_SCHEDULE = True # True if you want to use a lr_scheduler
 DEVICES = 2 #No of GPU devices
+NODES = 1 # No of compute devices
 
 # ==============================================================================
 # SimCLR Defaults
--- src/ssl_models/simsiam_train.py
+++ src/ssl_models/simsiam_train.py
@@ -1,57 +1,68 @@
 # ==============================================================================
-# Training code for SimSiam
-# Base line setting
-#   optimizer : SGD, momentum = 0.9
-#   learning rate :  base lr = 0.5 , lr = lr * BS/256
-#   Cosine Decay : 0.0001
-#   BS : 512
+# SimSiam Paper Implementation Details : Changes reflected on right after "|"
+
+# Optimizer 
+#   optimizer : SGD, momentum = 0.9 ; weight_decay = 0.0001
+#   learning rate :  base lr = 0.05 ; lr = base_lr * BS/256
+#   cosine decay | We have used CosineAnnealingLR from pytorch which maybe slightly different
+# Batch Size : 512 | Due to computational restraints (Even with 8 GPUS's) we have set to 256
+# Projection & Prediction Heads
 #   Projection Head : input_size = 2048, output_size = 2048, hidden_size = 2048
 #   Prediction Head : Input_size = 2048, Output_size = 2048, hidden_size = 512
-
-#todo : Need to incorporate cosine decay
-#todo : lr = lr * BS/256 , where base_lr = 0.5
-#! Cannot set batch size to 512 too big for memory
+# Loss Function : negative cosine similarity
+# Default Backbone : RESNET-50
+#   100 Epoch pre-training ablation study | Instead we use Pre-Trained model on RSP
 # ==============================================================================
 
-# ---------------------------------- imports --------------------------------- #
-
 import os
-import sys
 import math
 import argparse
 
 import torch 
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.utils.data import DataLoader
-import pytorch_lightning as pl
-from pytorch_lightning.loggers import CSVLogger
+from torch.utils.data import DataLoader, ConcatDataset
+from torch.optim.lr_scheduler import CosineAnnealingLR
+
+from torchvision.transforms import Compose
+
+import lightning.pytorch as pl
+from lightning.pytorch.loggers import CSVLogger
+from lightning.pytorch.callbacks import ModelCheckpoint
 
 from lightly.loss import NegativeCosineSimilarity
 from lightly.models.modules import SimSiamPredictionHead, SimSiamProjectionHead
 from lightly.transforms import SimSiamTransform
 from lightly.data import LightlyDataset
 
-from utils import load_model_weights
+from ssl_utils import load_model_weights
+import sys
+
 import config
 
-sys.path.append("RSP/Scene Recognition")
-from models.resnet import resnet50
+from torchvision.models import resnet50
 
-
-# ------------------------------- SimSIam Model ------------------------------ #
 class SimSiam(pl.LightningModule):
-    def __init__(self, resnet_hidden_dims, proj_hidden_dims, pred_hidden_dims, out_dims):
+    def __init__(self, model_params:dict, backbone:torch.nn.Sequential):
         super().__init__()
-        resnet = load_model_weights(
-            resnet50, 
-            path_to_weights = "models/rsp_weights/rsp-aid-resnet-50-e300-ckpt.pth", 
-            num_classes = 51
-        )
-        self.backbone = nn.Sequential(*list(resnet.children())[:-1])
-        self.projection_head = SimSiamProjectionHead(resnet_hidden_dims,proj_hidden_dims,out_dims) #These are the default values
-        self.prediction_head = SimSiamPredictionHead(out_dims,pred_hidden_dims,out_dims) #These are deafult values, bottleneck created in original paper
-        self.out_dims = out_dims # we need access to out_dims for computing collapse levels
+        self.projection_head = SimSiamProjectionHead(
+            input_dim = model_params["proj_input_dim"],
+            hidden_dim = model_params["proj_hidden_dim"],
+            output_dim = model_params["proj_output_dim"]
+        ) # 2048 > 2048 > 2048
+        self.prediction_head = SimSiamPredictionHead(
+            input_dim = model_params["pred_input_dim"],
+            hidden_dim = model_params["pred_hidden_dim"],
+            output_dim = model_params["pred_output_dim"]
+        ) #2048 > 512 > 2048
+        self.out_dims = model_params["pred_output_dim"] # we need access to out_dims for computing collapse levels
+        
+        #model parameters
+        self.model_params = model_params
+
+        # backbone
+        self.backbone = backbone
+
         # Loss
         self.criterion = NegativeCosineSimilarity()
 
@@ -59,84 +70,163 @@ class SimSiam(pl.LightningModule):
         self.avg_loss = 0.0
         self.avg_output_std = 0.0
         self.loss = 0.0
-
+        
 
     def forward(self, X):
-        f = self.backbone(X).flatten(start_dim = 1) #(b,3,256,256) -> (b,512,1,1) -> (b,512)
-        z = self.projection_head(f) #(b,2048)
-        p = self.prediction_head(z) #(b,2048)
-        z = z.detach() #We stop the gradient to prevent collapse
+        f = self.backbone(X) #(b,3,256,256) -> ... -> (b,2048,1,1) 
+        f = f.flatten(start_dim = 1) #(b,2048,1,1) -> (b,2048)
+        z = self.projection_head(f) #(b,2048) -> (b,2048) -> (b,2048) -> (b, 2048)
+        p = self.prediction_head(z) #(b,2048) -> (b,512) -> (b,2048)
+        z = z.detach() #SimSiams stop the gradient to prevent collapse
         return z, p
-
+    
     def training_step(self, batch, batch_idx):
+        # lightlyDataset passes X,y,path = batch, augmented versions X0,X1 = X
         (X0,X1) = batch[0]
         z0,p0 = self.forward(X0) #(b,2048),(b,2048)
         z1,p1 = self.forward(X1) #(b,2048),(b,2048)
         # Compute loss
         self.loss = 0.5 * (self.criterion(z0,p1) + self.criterion(z1, p0))
-        
-        # ======================================================================
-        #! comment out if you DONT need to compute collapse
-        # calculate the per-dimensional standard deviation of the outputs 
-        # we use this later to check whether the embeddings are collapsing
-        output = p0.detach() #(b, 512)
-        output = F.normalize(output, dim = 1)
-        output_std = torch.std(output, 0) #(2048,)
-        output_std = output_std.mean() # () <- float value
+        # Compute Collapse
+        if self.model_params["compute_collapse?"]:
+            self.compute_std_per_dim(p0)
 
+        return self.loss 
+    
+    def on_train_epoch_end(self):
+        self.log("training_loss", self.loss)
+        if self.model_params["apply_lr_scheduler?"]:
+            self.log("current_lr", self.learning_rate_scheduler.get_lr()[0])
+        if self.model_params["compute_collapse?"]:
+            self.log("avg_output_std", self.avg_output_std)
+            collapse_level = max(0.0, 1 - math.sqrt(self.out_dims) * self.avg_output_std)
+            self.log("collapse_level", collapse_level)
+
+    
+    def compute_std_per_dim(self, p0):
+        """Calculate per dimension std of outputs which we will use 
+        to check whether embeddings collapse"""
+        #output = p0.copy() # This is the prediction from left network
+        output = p0.clone().detach() #(b, 2048)
+        output = F.normalize(output, dim = 1) #(b, 2048)
+        output_std = torch.std(output,dim = 0) #(2048,)
+        output_std = output_std.mean() #(,) 
         # Check if embeddings are collapsing : Use a moving average to track the loss & std
         w = 0.9
         self.avg_loss = w * self.avg_loss + (1-w) * self.loss.item()
         self.avg_output_std = w * self.avg_output_std + (1-w) * output_std.item()
-        # ======================================================================
-        
-        return self.loss
-
-    def compute_std(self,p0,loss):
-        pass
-
-    def on_train_epoch_end(self):
-        # the level if collapse is karge uf the standard deviation if the 12 normalised
-        # outputs are is much smaller than 1 / sqrt(dim)
-        collapse_level = max(0.0, 1 - math.sqrt(self.out_dims) * self.avg_output_std)
-        self.log("collapse_level", collapse_level)
-        self.log("avg_output_std", self.avg_output_std)
-        self.log("training loss", self.loss)
-
 
     def configure_optimizers(self):
-        optim = torch.optim.SGD(self.parameters(), lr = 0.001)
-        return optim
+        if self.model_params["apply_lr_scheduler?"]:
+            base_lr = 0.05
+            lr = base_lr * self.model_params["batch_size"] / 256
+            optimizer = torch.optim.SGD(params = self.parameters(),lr = lr, weight_decay = 0.0001)
+            self.learning_rate_scheduler = CosineAnnealingLR(optimizer, T_max = config.MAX_EPOCHS)
+            return [optimizer], [{"scheduler":self.learning_rate_scheduler, "interval" : "epoch"}]
+        else:
+            return torch.optim.SGD(params = self.parameters(), lr = config.LR)
+            
 
 if __name__ == "__main__":
-    # ------------------------------ Argument Parse ------------------------------ #
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-dfold", "--data_folder", type = str)
+    # ------------------------------ Argument Parser ----------------------------- #
+    
+    parser = argparse.ArgumentParser(description = "Train BYOL algorithm") 
+    parser.add_argument(
+        "-data_fold_drn", 
+        type = str, 
+        help = "Path to drone data folder", 
+        default = "data/processed/sshsph_drn/drn_c3_256x_pch"
+    )
+    parser.add_argument(
+        "-data_fold_sat", 
+        type = str, 
+        help = "Path to sentinel data folder",
+        default = "data/interim/gee_sat/sen2a_c3_256x_clp0.3_uint8_ucln_pch"
+    )
+    parser.add_argument(
+        "-pretrain_weights_file", 
+        type = str, 
+        help = "Path to pretrained weights file", 
+        default="model_weights/pretrain_weights/rsp-aid-resnet-50-e300-ckpt.pth"
+    )
+    parser.add_argument(
+        "-save_weights_fold", 
+        type = str, 
+        help = "Path to where model weights + stats are saved", 
+        default = "model_weights/ssl_weights"
+    )
     args = parser.parse_args()
-    path_to_data = args.data_folder
 
-    # ---------------------- Simiam Transforms + Data loader --------------------- #
-    transform = SimSiamTransform(input_size = 256, normalize = {"mean" : config.IMAGE_MEAN, "std" : config.IMAGE_STD})
-    trainset = LightlyDataset(path_to_data, transform = transform)
-    trainloader = DataLoader(trainset, batch_size = config.BATCH_SIZE, shuffle = True, drop_last = True)
+    # ----------------------------- Model Parameters ----------------------------- #
+
+    model_params = {
+        "apply_lr_scheduler?" : True,
+        "compute_collapse?" : True,
+        "lr" : config.LR,
+        "max_epochs" : config.MAX_EPOCHS,
+        "batch_size" : config.BATCH_SIZE,
+        "proj_input_dim" : 2048,
+        "proj_hidden_dim" : 2048,
+        "proj_output_dim" : 2048,
+        "pred_input_dim" : 2048,
+        "pred_hidden_dim" : 512,
+        "pred_output_dim" : 2048
+    }
+
+    # ------------------------------- Get Backbone ------------------------------- #
+
+    resnet = load_model_weights(
+        resnet50, 
+        path_to_weights=args.pretrain_weights_file, 
+        num_classes = 51
+    )
+    resnet_backbone = nn.Sequential(*list(resnet.children())[:-1])
+
+    # ---------------------------- SimSiam Transforms ---------------------------- #
+    drn_transforms = SimSiamTransform(
+        input_size = 256, 
+        normalize = {"mean" : config.drn_img_mean, "std" : config.drn_img_std}
+    )
+    sat_transforms = SimSiamTransform(
+        input_size = 256, 
+        normalize = {"mean" : config.sat_img_mean, "std" : config.sat_img_std}
+    )
+
+    # --------------------------- Dataset + DataLoader --------------------------- #
+
+    # DRONE Dataset
+    drn_trainset = LightlyDataset(input_dir = args.data_fold_drn, transform=Compose([drn_transforms])) # .__getitem__() returns -> view1,view2,fname
+    drn_trainloader = DataLoader(drn_trainset, batch_size=config.BATCH_SIZE)
+
+    # SENTINEL Dataset
+    sen2a_trainset = LightlyDataset(input_dir= args.data_fold_sat, transform=Compose([sat_transforms]))
+    sen2a_trainloader = DataLoader(sen2a_trainset, batch_size=config.BATCH_SIZE)
+
+    # COMBINE datasets
+    drnsen2a_trainset = ConcatDataset([drn_trainset, sen2a_trainset])
+    drnsen2a_trainloader = DataLoader(drnsen2a_trainset, batch_size=config.BATCH_SIZE)
 
     # ------------------------------- SimSiam Model ------------------------------ #
-    simsiam = SimSiam(
-        resnet_hidden_dims = 2048,
-        proj_hidden_dims = 2048,
-        pred_hidden_dims = 512,
-        out_dims = 2048
-    )
-
+    simsiam = SimSiam(model_params,resnet_backbone)
+    
     # --------------------------------- Training --------------------------------- #
-    #todo Needs updating 
-    logger = CSVLogger("models/ssl_weights", name = f"simsiam-is{config.INPUT_SIZE}-bs{config.BATCH_SIZE}-ep{config.MAX_EPOCHS}")
+    save_name = f"simsiam-is{config.INPUT_SIZE}-bs{config.BATCH_SIZE}-ep{config.MAX_EPOCHS}-bb{'Res'}-ds{'DrnSen2a'}-cl{'ClUcl'}-nm{'TTDrnSat'}"
+    logger = CSVLogger(save_dir = args.save_weights_fold, name = save_name)
+    checkpoint_callback = ModelCheckpoint(
+        dirpath=os.path.join(args.save_weights_fold, save_name), 
+        filename="epoch:{epoch}",
+        save_on_train_epoch_end=True,
+        save_weights_only = True,
+        save_top_k = -1
+    )
     trainer = pl.Trainer(
-        default_root_dir = f"models/ssl_weights/simsiam-is{config.INPUT_SIZE}-bs{config.BATCH_SIZE}-ep{config.MAX_EPOCHS}",
-        devices = 1,
+        default_root_dir = os.path.join(args.save_weights_fold, save_name),
+        devices = config.DEVICES,
+        num_nodes= config.NODES,
         accelerator = "gpu",
         max_epochs = config.MAX_EPOCHS,
-        logger = logger
+        logger = logger,
+        callbacks = [checkpoint_callback]
     )
 
-    trainer.fit(simsiam, trainloader)
+    trainer.fit(simsiam, drnsen2a_trainloader)
